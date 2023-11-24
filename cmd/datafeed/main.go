7033f61cee86373e038b233ebc82476b2b1a7974
FergusJJ <fergus2004johnson@gmail.com>
2023-11-24 15:29:30 +0000

    initial

---
+++ cmd/datafeed/main.go
@@ -0,0 +1,12 @@
+package main
+
+/*
+  this service is responsible for connecting to external apis
+  it should process data so it is suitable to send to grpc servic
+*/
+
+func main() {
+  var exitcode = 0;
+
+  log.Println("exited with code", exitcode)
+}
---
+++ cmd/streamingservice/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"log"
+	"net"
+  "github.com/FergusJJ/realtimeFinancialData/internal/proto/pb"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+/*
+  This is a grpc responsible for sending data to clients over grpc
+  it should receive data from the datafeed service
+*/
+
+type server struct {
+
+}
+
+func main(){
+  listener, err := net.Listen("tcp", ":8080")
+  if err != nil {
+    log.Fatalln("failed to create listener: ",err)
+  }
+  s := grpc.NewServer()
+  reflection.Register(s)
+
+  pb.RegisterFinancialDataServer(s, &server{})
+  if err := s.Serve(listener); err != nil {
+    log.Fatalln("failed to serve:", err)
+  }
+}
